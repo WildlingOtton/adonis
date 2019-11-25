52c4cd051c5cae80548f8f9cd31fbbb82e468a23
Chandler Foster <otton75@hotmail.com>
2019-11-24 17:02:22 -0700

    Revert "Update header.php"
    This reverts commit e600bec74855458e457f34881bedab084c16d845.

--- web/header.php
+++ web/header.php
@@ -16,11 +16,11 @@
   else $loggedin = FALSE;
 
   echo "<title>$appname$userstr</title><link rel='stylesheet' " .
-       "href='styles.css' type='text'>"                     .
+       "href='css/styles.css' type='text/css'>"                     .
        "</head><body><center><canvas id='logo' width='624' "    .
        "height='96'>$appname</canvas></center>"             .
        "<div class='appname'>$appname$userstr</div>"            .
-       "<script src='javascript.js'></script>";
+       "<script src='js/javascript.js'></script>";
 
   if ($loggedin)
   {
