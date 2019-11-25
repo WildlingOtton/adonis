b944ac1e7692da74b6880155059dd6ca7390389d
Chandler Foster <otton75@hotmail.com>
2019-11-24 17:04:01 -0700

    barbell gif fix

--- web/js/javascript.js
+++ web/js/javascript.js
@@ -3,7 +3,7 @@ context              = canvas.getContext('2d')
 context.font         = 'bold italic 97px Georgia'
 context.textBaseline = 'top'
 image                = new Image()
-image.src            = 'images/barbell.gif'
+image.src            = 'barbell.gif'
 
 image.onload = function()
 {
