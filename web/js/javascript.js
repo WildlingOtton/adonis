0258b78194ee8e600c45a02faff4f499f26c7dd7
Chandler Foster <otton75@hotmail.com>
2019-11-24 17:06:29 -0700

    barbfill try 2

--- web/js/javascript.js
+++ web/js/javascript.js
@@ -3,7 +3,7 @@ context              = canvas.getContext('2d')
 context.font         = 'bold italic 97px Georgia'
 context.textBaseline = 'top'
 image                = new Image()
-image.src            = 'barbell.gif'
+image.src            = 'web/images/barbell.gif'
 
 image.onload = function()
 {
